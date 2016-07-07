2d2c25e2b5b7021d61f9cedd172d645ec8a164b2
Timothy Click <tclick@alumni.ou.edu>
2016-07-07 15:54:25 +0800

    Changed to format mini-language in strings.

--- scaProcessMSA.py
+++ scaProcessMSA.py
@@ -71,8 +71,7 @@ if __name__ == '__main__':
     parser.add_argument("-c", "--chainID", dest="chainID", default='A',
                         help="chain ID in the PDB for the reference sequence")
     parser.add_argument("-f", "--species", dest="species", help="species of the reference sequence")
-    parser.add_argument(
-        "-r", "--refseq", dest="refseq", help="reference sequence, supplied as a fasta file")
+    parser.add_argument("-r", "--refseq", dest="refseq", help="reference sequence, supplied as a fasta file")
     parser.add_argument("-o", "--refpos", dest="refpos",
                         help="reference positions, supplied as a text file with one position specified per line")
     parser.add_argument("-i", "--refindex", dest="i_ref", type=int,
@@ -85,8 +84,7 @@ if __name__ == '__main__':
                         help="truncate the alignment to the positions in the reference PDB, default: False")
     parser.add_argument("-m", "--matlab", action="store_true", dest="matfile", default=False,
                         help="write out the results of this script to a matlab workspace for further analysis")
-    parser.add_argument(
-        "--output", dest="outputfile", default=None, help="specify an outputfile name")
+    parser.add_argument("--output", dest="outputfile", default=None, help="specify an outputfile name")
     options = parser.parse_args()
 
     # A little bit of error checking/feedback for the user.
@@ -95,8 +93,7 @@ if __name__ == '__main__':
             print_("No PDBid, ignoring species...")
             options.species = None
         if options.refseq is not None and options.refpos is None:
-            print_(
-                "Using reference sequence but no position list provided! Just numbering positions 1 to length(sequence)")
+            print_("Using reference sequence but no position list provided! Just numbering positions 1 to length(sequence)")
             if options.pdbid is not None:
                 print_("And...ignoring the PDB file...")
                 options.pdbid = None
@@ -111,8 +108,7 @@ if __name__ == '__main__':
 
     # Read in initial alignment
     headers_full, sequences_full = sca.readAlg(options.alignment)
-    print_('Loaded alignment of %i sequences, %i positions.' %
-           (len(headers_full), len(sequences_full[0])))
+    print_('Loaded alignment of {:d} sequences, {:d} positions.'.format(len(headers_full), len(sequences_full[0])))
 
     # Check the alignment and remove sequences containing non-standard amino acids
     print_("Checking alignment for non-standard amino acids")
@@ -128,8 +124,7 @@ if __name__ == '__main__':
             hd_out.append(headers_full[i])
     headers_full = hd_out
     sequences_full = alg_out
-    print_("Aligment size after removing sequences with non-standard amino acids: %i" %
-           (len(sequences_full)))
+    print_("Aligment size after removing sequences with non-standard amino acids: {:d}".format(len(sequences_full)))
 
     # Do an initial trimming to remove excessively gapped positions - this is
     # critical for building a correct ATS
@@ -137,7 +132,7 @@ if __name__ == '__main__':
     alg_out, poskeep = sca.filterPos(sequences_full, [1], 0.8)
     sequences_ori = sequences_full
     sequences_full = alg_out
-    print_("Alignment size post-trimming: %i positions" % len(sequences_full[0]))
+    print_("Alignment size post-trimming: {:d} positions".format(len(sequences_full[0])))
 
     # If i_ref is directly provided, we use it, ignoring all else.  Otherwise,
     # we explore the other ways of specifying a reference sequences: (1)
@@ -158,16 +153,15 @@ if __name__ == '__main__':
                         i_ref = sca.MSAsearch(
                             headers_full, sequences_full, seq_pdb, options.species)
                         Options_ref = i_ref
-                        print_("reference sequence index is: %i" % (i_ref))
+                        print_("reference sequence index is: {:d}".format(i_ref))
                         print_(headers_full[i_ref])
                         print_(sequences_full[i_ref])
                     except:
-                        print_(
-                            "Cant find the reference sequence using species-based best_match! Using global MSAsearch...")
+                        print_("Cant find the reference sequence using species-based best_match! Using global MSAsearch...")
                         try:
                             i_ref = sca.MSAsearch(headers_full, sequences_full, seq_pdb)
                             options.i_ref = i_ref
-                            print_("reference sequence index is: %i" % (i_ref))
+                            print_("reference sequence index is: {:d}".format(i_ref))
                             print_(headers_full[i_ref])
                             print_(sequences_full[i_ref])
                         except:
@@ -177,13 +171,12 @@ if __name__ == '__main__':
                         print_("Finding reference sequence using global MSAsearch...")
                         i_ref = sca.MSAsearch(headers_full, sequences_full, seq_pdb)
                         options.i_ref = i_ref
-                        print_("reference sequence index is: %i" % (i_ref))
+                        print_("reference sequence index is: {:d}".format(i_ref))
                         print_(headers_full[i_ref])
                         print_(sequences_full[i_ref])
                     except:
                         sys.exit("Error!!  Can't find reference sequence...")
-                sequences, ats = sca.makeATS(
-                    sequences_full, ats_pdb, seq_pdb, i_ref, options.truncate)
+                sequences, ats = sca.makeATS(sequences_full, ats_pdb, seq_pdb, i_ref, options.truncate)
                 dist_new = np.zeros((len(ats), len(ats)))
                 for (j, pos1) in enumerate(ats):
                     for (k, pos2) in enumerate(ats):
@@ -203,7 +196,7 @@ if __name__ == '__main__':
                 h_tmp, s_tmp = sca.readAlg(options.refseq)
                 i_ref = sca.MSAsearch(headers_full, sequences_full, s_tmp[0])
                 options.i_ref = i_ref
-                print_("reference sequence index is: %i" % (i_ref))
+                print_("reference sequence index is: {:d}".format(i_ref))
                 print_(headers_full[i_ref])
                 if options.refpos is not None:
                     try:
@@ -223,12 +216,11 @@ if __name__ == '__main__':
         else:
             msa_num = sca.lett2num(sequences_full)
             i_ref = sca.chooseRefSeq(sequences_full)
-            print_("No reference sequence given, chose as default (%i): %s" %
-                   (i_ref, headers_full[i_ref]))
+            print_("No reference sequence given, chose as default ({:d}): {}".format(i_ref, headers_full[i_ref]))
             sequences = sequences_full
             ats = range(len(sequences[0]))
     else:
-        print_("using provided reference index %i" % (i_ref))
+        print_("using provided reference index {:d}".format(i_ref))
         print_(headers_full[i_ref])
         s_tmp = sequences_ori[i_ref]
         try:
@@ -244,17 +236,16 @@ if __name__ == '__main__':
             sys.exit("Error!!  Can't find reference sequence...")
 
     # filtering sequences and positions, calculations of effective number of seqs
-    print_("Conducting sequence and position filtering: alignment size is %i seqs, %i pos" %
-           (len(sequences), len(sequences[0])))
+    print_("Conducting sequence and position filtering: alignment size is {:d} seqs, {:d} pos".format(len(sequences),
+                                                                                                      len(sequences[0])))
     if options.pdbid is not None:
-        print_("ATS and distmat size - ATS: %i, distmat: %i x %i"
-               % (len(ats), len(dist_pdb), len(dist_pdb[0])))
+        print_("ATS and distmat size - ATS: {:d}, distmat: {:d} x {:d}".format(len(ats), len(dist_pdb), len(dist_pdb[0])))
     else:
-        print_("ATS should also have %i positions - ATS: %i" % (len(sequences[0]), len(ats)))
+        print_("ATS should also have {:d} positions - ATS: {:d}".format(len(sequences[0]), len(ats)))
 
     if i_ref is not None:
-        alg0, seqw0, seqkeep = sca.filterSeq(sequences, i_ref, max_fracgaps=options.parameters[1], min_seqid=options.parameters[2],
-                                             max_seqid=options.parameters[3])
+        alg0, seqw0, seqkeep = sca.filterSeq(sequences, i_ref, max_fracgaps=options.parameters[1],
+                                             min_seqid=options.parameters[2], max_seqid=options.parameters[3])
     else:
         alg0, seqw0, seqkeep = sca.filterSeq(sequences, max_fracgaps=options.parameters[1], min_seqid=options.parameters[2],
                                              max_seqid=options.parameters[3])
@@ -266,8 +257,8 @@ if __name__ == '__main__':
         distmat = dist_pdb[np.ix_(iposkeep, iposkeep)]
     effseqsprelimit = int(seqw0.sum())
     Nseqprelimit = len(alg1)
-    print_("After filtering: alignment size is %i seqs, %i effective seqs, %i pos" %
-           (len(alg1), effseqsprelimit, len(alg1[0])))
+    print_("After filtering: alignment size is {:d} seqs, {:d} effective seqs, {:d} pos".format(len(alg1), effseqsprelimit,
+                                                                                                len(alg1[0])))
 
     # Limitation of total sequences to [1.5 * # ofeffective sequences] if Nselect is set to True
     if (options.Nselect):
@@ -284,15 +275,15 @@ if __name__ == '__main__':
     msa_num = sca.lett2num(alg)
     Nseq, Npos = msa_num.shape
     print_("Final alignment parameters:")
-    print_("Number of sequences: M = %i" % (Nseq))
-    print_("Number of effective sequences: M' = %i" % (effseqs))
-    print_("Number of alignment positions: L = %i" % (Npos))
+    print_("Number of sequences: M = {:d}".format(Nseq))
+    print_("Number of effective sequences: M' = {:.0f}".format(np.round(effseqs)))
+    print_("Number of alignment positions: L = {:d}".format(Npos))
 
     if options.pdbid is not None:
-        print_("Number of positions in the ats: %i" % (len(ats)))
+        print_("Number of positions in the ats: {:d}".format(len(ats)))
         structPos = [i for (i, k) in enumerate(ats) if k != '-']
-        print_("Number of structure positions mapped: %i" % (len(structPos)))
-        print_("Size of the distance matrix: %i x %i" % (len(distmat), len(distmat[0])))
+        print_("Number of structure positions mapped: {:d}".format(len(structPos)))
+        print_("Size of the distance matrix: {:d} x {:d}".format(len(distmat), len(distmat[0])))
 
     # saving the important stuff. Everything is stored in a file called
     # [MSAname]_sequence.db.  But we will also write out the final processed
@@ -301,10 +292,10 @@ if __name__ == '__main__':
     path_list = options.alignment.split(os.sep)
     fn = path_list[-1]
     fn_noext = fn.split(".")[0]
-    with open("Outputs/" + fn_noext + "processed" + ".fasta", "w") as f:
+    with open(".".join((path.join("Outputs", fn_noext + "processed"), "fasta")), "w") as f:
         for i in range(len(alg)):
             #f.write(">" + hd[i] + "\n")
-            print_(">%s" % (hd[i]), file=f)
+            print_(">{}".format(hd[i]), file=f)
             print_(alg[i], file=f)
 
     D = {}
@@ -333,7 +324,7 @@ if __name__ == '__main__':
 
     if (options.outputfile is not None):
         fn_noext = options.outputfile
-    print_("Opening database file " + "Outputs/" + fn_noext)
+    print_(" ".join(("Opening database file", path.join("Outputs", fn_noext))))
     db = {}
     db['sequence'] = D
 
@@ -341,5 +332,6 @@ if __name__ == '__main__':
         matfile = path.join("Outputs", fn_noext)
         savemat(matfile, db, appendmat=True, oned_as='column')
 
-    with open(".".join((path.join("Outputs", fn_noext), "db")), "wb") as db_file:
+    db_filename = ".".join((path.join("Outputs", fn_noext), "db"))
+    with open(db_filename, "wb") as db_file:
         pickle.dump(db, db_file, protocol=pickle.HIGHEST_PROTOCOL)
